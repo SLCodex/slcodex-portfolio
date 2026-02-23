2d70dc5ad025316fbd59e00a0d50e864d20ce692
SLCodex <kidochan87@gmail.com>
2026-02-23 11:17:27 +0800

    'prod'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,7 +30,7 @@
             }
         </style>
 
-        <title inertia>{{ config('app.name', 'SLCodex') }}</title>
+        <title inertia>{{ config('app.name') }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
